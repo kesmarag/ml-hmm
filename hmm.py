9b3b28ce4615c2c9533a0b34d0353e8fa06ff8d1
Costas Smaragdakis <kesmarag@tem.uoc.gr>
2018-03-09 14:02:00 +0200

    update

--- hmm.py
+++ hmm.py
@@ -421,7 +421,7 @@ class HMM(object):
       # print('gamma_mv shape : ', gamma_mv.get_shape())
       xi_mv = tf.reduce_mean(self._xi, axis=1, name='xi_mv')
       # update the initial state probabilities
-      self._p0_tf_new = tf.transpose(tf.expand_dims(gamma_mv[0], -1))
+      self._p0_tf_new = self._p0_tf #  tf.transpose(tf.expand_dims(gamma_mv[0], -1))
       # update the transition matrix
       # first calculate sum_n=2^{N} xi_mean[n-1,k , n,l]
       sum_xi_mean = tf.squeeze(tf.reduce_sum(xi_mv, axis=0))
