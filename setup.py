d5c3da88e362d438768bc4757e34875ac3ccd372
Costas Smaragdakis <kesmarag@gmail.com>
2018-03-25 16:08:15 +0300

    version 0.0.4 (tensorflow 1.5.1)

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='kesmarag-ml-hmm',
-      version='0.0.3',
+      version='0.0.4',
       description='An implementation of the Gaussian Hidden Markov Model on top of TensorFlow',
       author='Costas Smaragdakis',
       author_email='kesmarag@gmail.com',
       url='https://github.com/kesmarag/ml-hmm',
       packages=['kesmarag.ml.hmm'],
       package_dir={'kesmarag.ml.hmm': './'},
-      install_requires=['tensorflow==1.5.0',
+      install_requires=['tensorflow==1.5.1',
                         'numpy>=1.12.1',
                         'scikit-learn>=0.18.1',
                         'kesmarag-ml-utils'], )
