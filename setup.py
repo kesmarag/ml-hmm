831d1868bb06eea4b607c2fdfa68ab1e4e471d2b
Costas Smaragdakis <kesmarag@gmail.com>
2018-06-29 22:26:34 +0300

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='kesmarag-ml-hmm',
-      version='0.1.1',
+      version='0.1.2',
       description='An implementation of the Gaussian Hidden Markov Model on top of TensorFlow',
       author='Costas Smaragdakis',
       author_email='kesmarag@gmail.com',
